218a2006669258b32ef957e69ef151581ff50738
lemerafael <lemerafael@gmail.com>
2025-11-16 11:24:14 -0300

    Remove duplicate SpringBootApplication and update the mocked class for use with Swagger

--- src/main/java/com/sky/projects/DemoApplication.java
+++ src/main/java/com/sky/projects/DemoApplication.java
@@ -4,39 +4,38 @@ import java.util.List;
 import java.util.logging.Level;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
+import com.sky.projects.model.dto.UserDTO;
 import com.sky.projects.model.persistence.ExternalProject;
 import com.sky.projects.model.persistence.User;
 import com.sky.projects.repository.ExternalProjectRepository;
-import com.sky.projects.repository.UserRepository;
+import com.sky.projects.service.UserManagementService;
 
 import lombok.extern.java.Log;
 
 @Log
 @SpringBootApplication
 public class DemoApplication {
-    @Autowired 
-    private UserRepository userRepo;
+    @Autowired
+    private UserManagementService userService;
 
     @Autowired
     private ExternalProjectRepository projectRepo;
-  
+
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterStartup() {
-        List<User> allUsers = this.userRepo.findAll(); 
+        List<User> allUsers = this.userService.getAllUsers();
         log.log(Level.INFO, "Number of users: " + allUsers.size());
- 
-        User newUser = new User(); 
-        newUser.setName("John"); 
-        newUser.setPassword("pass"); 
-        newUser.setEmail("email@email.com"); 
-        log.log(Level.INFO, "Saving new user..."); 
-        newUser = this.userRepo.save(newUser); 
- 
-        allUsers = this.userRepo.findAll(); 
+
+        UserDTO newUserReq = new UserDTO("email@email.com", "pass", "John");
+        log.log(Level.INFO, "Saving new user...");
+        User newUser = this.userService.createUser(newUserReq);
+
+        allUsers = this.userService.getAllUsers();
         log.log(Level.INFO, "Number of users: " + allUsers.size());
 
         List<ExternalProject> allProjects = this.projectRepo.findAll();
@@ -46,10 +45,14 @@ public class DemoApplication {
         project.setId(1);
         project.setUser(newUser);
         project.setName("Project");
-        log.log(Level.INFO, "Saving new project..."); 
+        log.log(Level.INFO, "Saving new project...");
         this.projectRepo.save(project);
 
         allProjects = this.projectRepo.findAll();
         log.log(Level.INFO, "Number of projects: " + allProjects.size());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
